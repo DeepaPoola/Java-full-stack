05fb199e9c134d1e8e1e904518c6e81588e70551
Poola Deepa <poola.deepa298@gmail.com>
2023-07-14 15:17:27 +0530

    ok

---
+++ Practice/Javascript/Function/five.js
@@ -0,0 +1,7 @@
+function emp(id,name,sal=5000)
+{
+    console.log(id)
+    console.log(name)
+    console.log(sal+5000)
+}
+emp(10,"Deepa",30000)
---
+++ Practice/Javascript/Function/six.js
@@ -0,0 +1,12 @@
+/*function wish(msg)
+{
+    console.log(msg)
+    
+}
+wish("Hello")*/
+function wish(msg)
+{
+    return "Hello GM"
+}
+let message=wish()
+console.log(message)
